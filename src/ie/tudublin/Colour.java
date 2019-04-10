fe0eb5bd6a1de1232d807952714ff019877a9637
Declan <Declan.thornee98@gmail.com>
2019-04-10 17:21:25 +0100

    Fixed Colour class constructor

--- src/ie/tudublin/Colour.java
+++ src/ie/tudublin/Colour.java
@@ -19,7 +19,13 @@ public class Colour
     //Constructor that takes inital values & assigns them to the fields.
     public Colour(TableRow tr)
     {
-        this(tr.getString("colour"), tr.getFloat("r"), tr.getFloat("g"), tr.getFloat("b"), tr.getFloat("value"));
+        //this(tr.getString("colour"), tr.getFloat("r"), tr.getFloat("g"), tr.getFloat("b"), tr.getFloat("value"));
+        
+        colour = tr.getString("Display Name");
+        r = tr.getFloat("r");
+        g = tr.getFloat("g");
+        b = tr.getFloat("Zg");
+        value = tr.getFloat("value");
     }
 
 
@@ -41,6 +47,62 @@ public class Colour
     public String toString()
     {
         return r + "\t" + "\t" + b + "\t" + g + "\t" + value + "\t" + colour;  
+    }
+
+    /**
+     * @return the r
+     */
+    public float getR() {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(float r) {
+        this.r = r;
+    }
+
+    /**
+     * @return the g
+     */
+    public float getG() {
+        return g;
+    }
+
+    /**
+     * @param g the g to set
+     */
+    public void setG(float g) {
+        this.g = g;
+    }
+
+    /**
+     * @return the b
+     */
+    public float getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(float b) {
+        this.b = b;
+    }
+
+    /**
+     * @return the value
+     */
+    public float getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(float value) {
+        this.value = value;
     }
 
 }
